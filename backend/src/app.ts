a7e8c48884c9a23fdd1bc4f661c0f924823bbbaf
Ivan Spirkovski <Ivan.Spirkovski@netcetera.com>
2021-12-24 09:59:37 +0100

    Prettier-eslint config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello world!'
-    });
+  res.json({ message: 'Hello world!' });
 });
