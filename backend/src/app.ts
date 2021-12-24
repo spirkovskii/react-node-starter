95130d8ae2c8a270e89edb2f53a12b95a34eb181
Ivan Spirkovski <Ivan.Spirkovski@netcetera.com>
2021-12-24 09:43:48 +0100

    Use bodyparser for parsing requests and urls

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,16 @@
 import express, { Application } from 'express'
 import * as path from 'path';
-
+import bodyParser from "body-parser";
 export const app: Application = express()
+
+app.set('port', process.env.PORT || 3001);
 app.use(express.static(path.resolve(__dirname, '../../frontend/build')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.get("/", (req, res) => {
+    res.json({
+        message: 'Hello world!'
+    })
+})
