10e9bc0afc11d5ab494691de5694c0890b51cf79
Ivan Spirkovski <Ivan.Spirkovski@netcetera.com>
2021-12-24 09:48:54 +0100

    Eslint config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
-import express, { Application } from 'express'
+import express, { Application } from 'express';
 import * as path from 'path';
-import bodyParser from "body-parser";
-export const app: Application = express()
+import bodyParser from 'body-parser';
+export const app: Application = express();
 
 app.set('port', process.env.PORT || 3001);
 app.use(express.static(path.resolve(__dirname, '../../frontend/build')));
@@ -9,8 +9,8 @@ app.use(express.static(path.resolve(__dirname, '../../frontend/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.json({
         message: 'Hello world!'
-    })
-})
+    });
+});
